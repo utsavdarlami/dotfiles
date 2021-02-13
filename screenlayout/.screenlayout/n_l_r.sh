611fdd23b56189afdb306c71bf0fef0fe66ea267
Utsav <utsavdarlami17@gmail.com>
2021-02-13 12:32:37 +0545

    left monitor right laptop

--- screenlayout/.screenlayout/n_l_r.sh
+++ screenlayout/.screenlayout/n_l_r.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --mode 1440x900 --pos 1920x0 --rotate normal --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 1440x0 --rotate normal --output DP-1-1 --mode 1440x900 --pos 0x0 --rotate normal --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
 cp ~/.config/bspwm/nvidia_double_rc ~/.config/bspwm/bspwmrc
 bspc wm -r
