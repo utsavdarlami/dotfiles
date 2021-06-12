8db7d12cfdac103f32fa1c86d995acde3b96c6bc
Utsav <utsavdarlami17@gmail.com>
2021-06-12 12:54:24 +0545

    arg of simulatiion

--- adoptAndSymlink.sh
+++ adoptAndSymlink.sh
@@ -1,5 +1,4 @@
 # !/bin/bash
-
 dirlist="
             alacritty 
             bash 
@@ -30,11 +29,13 @@ etc="
 echo $dirlist
 echo $etc
 
-# -nvt for simulation ,, to check if everythings is safely linked
-#stow --adopt -nvt ~ $dirlist
-#stow --adopt -nvt /etc/ $etc
-#stow * -nvt ~ $dirlist
-
-# -vt for actual linking
-stow --adopt -vt ~ $dirlist
-stow --adopt -vt /etc/ $etc
+if [ $1 = "test" ]; then
+    # -nvt for simulation ,, to check if everythings is safely linked
+    stow --adopt -nvt ~ $dirlist
+    stow --adopt -nvt /etc/ $etc
+    stow * -nvt ~ $dirlist
+else 
+    #stow -vt for actual linking
+    stow --adopt -vt ~ $dirlist
+    stow --adopt -vt /etc/ $etc
+fi
